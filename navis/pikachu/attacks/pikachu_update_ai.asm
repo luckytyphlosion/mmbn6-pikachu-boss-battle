537b4dcccb442dfff433924c95747962e33c3fb8
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2022-10-06 21:04:43 -0400

    Commit just to preserve elemtrap vdoll softlock, host side.

--- navis/pikachu/attacks/pikachu_update_ai.asm
+++ navis/pikachu/attacks/pikachu_update_ai.asm
@@ -90,24 +90,18 @@ pikachu_update_ai_update:
 	mov r0, 0
 	strb r0, [r4, oAIData_Unk_1d]
 	strb r0, [r4, oAIData_Unk_1e]
-	ldr r1, =0x203fffc
-	ldr r1, [r1]
-	mov r0, 46 + 0x10 ; elec cross beam
-	tst r1, r1
-	bne @@elecCrossBeam
-	mov r0, 8
-	strb r0, [r7, 0x3]
-	mov r0, 0x12
-@@elecCrossBeam:
+	mov r0, 0x15 ; elemtrap
 	bl object_setattack0
-	mov r0, 1
-	strb r0, [r7, 0xe]
+	mov r0, 0x14
+	strb r0, [r7, 0x3]	
+	;mov r0, 1
+	;strb r0, [r7, 0xe]
 	mov r0, 60 ; damage
 	str r0, [r7, 0x8]
-	ldr r0, =0x10f00
+	mov r0, 0
 	str r0, [r7, 0xc]
-	mov r0, 3
-	strb r0, [r7, 0x2]
+	;mov r0, 3
+	;strb r0, [r7, 0x2]
 @@done:
 	bl pikachu_track_movement
 	pop r4-r7, r15
